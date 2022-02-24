111c454a6046c12f1400018d11017583c36f8a5b
Kocsis Benedek <benedek0609@gmail.com>
2022-02-24 11:54:41 +0100

    Amending the last commit...

---
+++ test_project_1_blinky/test_project_1_blinky.srcs/sources_1/new/first.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.02.2022 18:45:02
+// Design Name: 
+// Module Name: first
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module first(
+
+    );
+endmodule
